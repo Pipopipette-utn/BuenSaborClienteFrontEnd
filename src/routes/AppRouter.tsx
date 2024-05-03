044a7c388ecc0c11262fd3720575f9a8f9f14a62
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-05-03 15:24:21 -0300

    Modificando ando

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -5,21 +5,42 @@ import { Menu } from "../components/screens/Menu/Menu";
 import { Pedido } from "../components/screens/Pedido/Pedido";
 import { Cuenta } from "../components/screens/Cuenta/Cuenta";
 import Footer from "../components/ui/Footer/Footer";
+import { useState } from "react";
+import { darkTheme } from "../components/Themes/DarkTheme";
+import { lightTheme } from "../components/Themes/LightTheme";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import "./button.css";
+import CustomSwitch from "../components/ui/CustomSwitch/CustomSwitch";
 
 export const AppRouter = () => {
+  const [currentTheme, setCurrentTheme] = useState(lightTheme); // State to manage the theme
   return (
-    <div>
-      <ResponsiveAppBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/pedido" element={<Pedido />} />
-        <Route path="/cuenta" element={<Cuenta />} />
-        {
-          //        <Route path="/categorias/:category" element={<Categorias />} />
-        }
-      </Routes>
-      <Footer />
-    </div>
+    <ThemeProvider theme={currentTheme}>
+      <div>
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/pedido" element={<Pedido />} />
+          <Route path="/cuenta" element={<Cuenta />} />
+          {
+            //        <Route path="/categorias/:category" element={<Categorias />} />
+          }
+        </Routes>
+        <Footer />
+        <CustomSwitch />
+        {currentTheme === lightTheme ? "Dark Mode" : "Light Mode"}
+      </div>
+    </ThemeProvider>
   );
 };
+/*
+        <Button
+          variant="contained"
+          className="floating-button"
+          onClick={toggleTheme}
+        >
+          {currentTheme === lightTheme ? "Dark Mode" : "Light Mode"}
+        </Button>
+
+*/
