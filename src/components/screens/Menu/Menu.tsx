d2a897caa555d04a3f5bd56332fba3a6878f7d12
DamianAguero329 <aguerodamian15@gmail.com>
2024-05-19 23:55:43 -0300

    MODIFICADO Menu y BD
    AGREGADO cardArticulo, SupaBaseClient, ModaLDetalle, .env y MODIFICADO Menu

--- src/components/screens/Menu/Menu.tsx
+++ src/components/screens/Menu/Menu.tsx
@@ -1,11 +1,41 @@
 import { MenuCategoria } from "../../ui/MenuCategorias/MenuCategorias";
+import React, { useEffect, useState } from 'react';
+import { IArticulo } from '../../../types/empresa';
+import { CardArticulo } from './CardArticulo';
+import supabase from "../../../services/SupabaseClient";
 
-export const Menu = () => {
+
+export const Menu: React.FC = () => {
+  const [articulos, setArticulos] = useState<IArticulo[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchArticulos = async () => {
+      const { data, error } = await supabase
+        .from<IArticulo>('articulos')
+        .select('*');
+
+      if (error) {
+        console.error('Error fetching articles:', error);
+      } else {
+        setArticulos(data || []);
+      }
+      setLoading(false);
+    };
+
+    fetchArticulos();
+  }, []);
+
+  if (loading) {
+    return <p>Cargando...</p>;
+  }
+
   return (
     <div>
-      Menu lateral con las categorias
-      <MenuCategoria categorias={[]} />
-      map de las card de los productos
+        <MenuCategoria categorias={[]} />
+      {articulos.map((articulo) => (
+        <CardArticulo key={articulo.id} articulo={articulo} />
+      ))}
     </div>
   );
 };
---
+++ src/components/ui/CardArticulo/CardArticulo.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { ModalDetalle } from './ModalDetalle';
+import { IArticulo } from '../../../types/empresa';
+
+
+interface CardArticuloProps {
+  articulo: IArticulo;
+}
+
+export const CardArticulo: React.FC<CardArticuloProps> = ({ articulo }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
+  
+  const obtenerImagenPrincipal = (articulo: IArticulo): string | undefined => {
+    return articulo.imagenes.length > 0 ? articulo.imagenes[0] : undefined;
+};
+
+  return (
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          imagen={obtenerImagenPrincipal}
+          titulo={articulo.denominacion}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {articulo.denominacion}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            ${articulo.precioVenta}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Agregar al carrito</Button>
+          <Button size="small" onClick={handleOpen}>Detalle</Button>
+        </CardActions>
+      </Card>
+      <ModalDetalle open={open} handleClose={handleClose} articulo={articulo} />
+    </>
+  );
+}
