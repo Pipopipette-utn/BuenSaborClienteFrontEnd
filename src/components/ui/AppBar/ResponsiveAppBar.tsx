b8295fe375745634deecf4e78a2fa2af6fbc7b15
DamianAguero329 <aguerodamian15@gmail.com>
2024-05-27 08:51:55 -0300

    MODIFICADO Appbar

--- src/components/ui/AppBar/ResponsiveAppBar.tsx
+++ src/components/ui/AppBar/ResponsiveAppBar.tsx
@@ -154,7 +154,7 @@ function ResponsiveAppBar() {
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
+            <Tooltip title="">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
