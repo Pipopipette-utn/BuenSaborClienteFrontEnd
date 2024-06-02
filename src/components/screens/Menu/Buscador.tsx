6b334657ef903ce4aa81ed356eb879544f23dcac
DamianAguero329 <aguerodamian15@gmail.com>
2024-06-02 17:38:19 -0300

    Update Buscador.tsx

--- src/components/screens/Menu/Buscador.tsx
+++ src/components/screens/Menu/Buscador.tsx
@@ -5,7 +5,6 @@ import SearchIcon from '@mui/icons-material/Search';
 
 export const Buscador = () => {
 
-
     const Search = styled('div')(({ theme }) => ({
         position: 'relative',
         borderRadius: theme.shape.borderRadius,
@@ -36,7 +35,6 @@ export const Buscador = () => {
         color: 'inherit',
         '& .MuiInputBase-input': {
           padding: theme.spacing(1, 1, 1, 0),
-          // vertical padding + font size from searchIcon
           paddingLeft: `calc(1em + ${theme.spacing(4)})`,
           transition: theme.transitions.create('width'),
           width: '100%',
