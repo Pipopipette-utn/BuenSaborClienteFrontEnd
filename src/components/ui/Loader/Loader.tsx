ec11808240632a480bb101350f073431cf661b0d
DamianAguero329 <aguerodamian15@gmail.com>
2024-06-02 21:40:32 -0300

    AGREGADO loader

---
+++ src/components/ui/Loader/Loader.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './PizzaLoader.css';
+
+const PizzaLoader: React.FC = () => {
+  return (
+    <div className="pizza-loader">
+      <div className="slice">
+        <div className="cheese"></div>
+        <div className="pepperoni pepperoni1"></div>
+        <div className="pepperoni pepperoni2"></div>
+        <div className="pepperoni pepperoni3"></div>
+        <div className="pepperoni pepperoni4"></div>
+      </div>
+      <div className="bite"></div>
+    </div>
+  );
+};
+
+export default PizzaLoader;
