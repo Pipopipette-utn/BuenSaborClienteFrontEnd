0205f5207aedb18e1e921e4eddc9819ad5618335
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-06-10 14:13:09 -0300

    fix!: multiple renderizado de CheckoutMP

--- src/components/MP/CheckoutMP.tsx
+++ src/components/MP/CheckoutMP.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect, useState, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/Store";
 import { PreferenceMp } from "../../types/PreferenceMP";
@@ -11,29 +10,36 @@ export const CheckoutMp: React.FC = () => {
   const items = useSelector((state: RootState) => state.cart.items);
   const [idPreference, setIdPreference] = useState<string>("");
 
-  const getPreferenceMP = async () => {
-    if (items.length > 0) {
-      const response: PreferenceMp = await createPreferenceMp(pedido!);
-      console.log("Response: ", response);
-      console.log("Response id: ", response.id);
-      console.log(pedido);
-      if (response) setIdPreference(response.id!);
-    } else {
-      alert("Agregue al menos un instrumento al carrito");
-    }
-  };
-
   useEffect(() => {
-    getPreferenceMP();
     initMercadoPago("TEST-bd8b9416-db7c-459e-8f5f-bb08cf78c8e0", {
       locale: "es-AR",
     }); // Credencial de prueba
-  }, []);
 
-  return (
+    const getPreferenceMP = async () => {
+      if (items.length > 0 && pedido) {
+        try {
+          const response: PreferenceMp = await createPreferenceMp(pedido);
+          console.log("Response: ", response);
+          console.log("Response id: ", response.id);
+          console.log(pedido);
+          if (response && response.id) {
+            setIdPreference(response.id);
+          }
+        } catch (error) {
+          console.error("Error creating preference: ", error);
+        }
+      } else {
+        alert("Agregue al menos un instrumento al carrito");
+      }
+    };
+
+    getPreferenceMP();
+  }, [items, pedido]);
+
+  return idPreference ? (
     <Wallet
       initialization={{ preferenceId: idPreference }}
       customization={{ texts: { valueProp: "smart_option" } }}
     />
-  );
+  ) : null;
 };
