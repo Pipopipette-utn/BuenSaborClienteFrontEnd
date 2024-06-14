810dd414188edce8b71aea136aaf469870b3a1d9
DamianAguero329 <aguerodamian15@gmail.com>
2024-06-14 00:59:00 -0300

    fix screen pedido

--- src/components/screens/Pedido/Pedido.tsx
+++ src/components/screens/Pedido/Pedido.tsx
@@ -1,18 +1,35 @@
-import Slider from "react-slick";
-import Sidebar from "../../ui/SideBar/Sidebar";
+import { useSelector } from 'react-redux';
 import { useFetch } from "../../../hooks/UseFetch";
 import { IEmpresa } from "../../../types/empresa";
-import { Carrito } from "../../ui/Carrito/Carrito";
+import { IDetallePedido } from "../../../types/pedido";
+import { RootState } from '../../../redux/Store';
 
 export const Pedido = () => {
   const { data: empresa, loading: loadingEmpresa } = useFetch<IEmpresa>(
     "http://localhost:8080/empresas/3"
   ); //debo pasarle el id desde otro lado
+
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+
   if (loadingEmpresa) return <h1>Cargando...</h1>;
+
   return (
     <>
       <h1>{empresa?.nombre}</h1>
-      <Carrito />
+      <div>
+        {cartItems.length === 0 ? (
+          <p>No hay artículos en el carrito.</p>
+        ) : (
+          <ul>
+            {cartItems.map((item: IDetallePedido) => (
+              <li key={item.articulo.id}>
+                <p>Artículo: {item.articulo.denominacion}</p>
+                <p>Cantidad: {item.cantidad}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </>
   );
 };
