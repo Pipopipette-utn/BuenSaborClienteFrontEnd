5b01106c6d67f4f1062046faa09019c3d7f791dd
DamianAguero329 <aguerodamian15@gmail.com>
2024-06-13 21:00:28 -0300

    CAMBIO jspdf por endpoint

--- src/components/screens/HistorialPedidos/HistorialPedidos.tsx
+++ src/components/screens/HistorialPedidos/HistorialPedidos.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Button, TextField, Paper, Typography, List, ListItem, ListItemText } from '@mui/material';
-import { jsPDF } from 'jspdf';
 import { IPedido } from '../../../types/pedido';
 
 export const HistorialPedidos: React.FC = () => {
@@ -9,10 +8,11 @@ export const HistorialPedidos: React.FC = () => {
   const [filterDate, setFilterDate] = useState<string>('');
 
   useEffect(() => {
-    get('/api/pedidos')
-      .then(response => {
-        setPedidos(response.data);
-        setFilteredPedidos(response.data);
+    fetch('/pedidos')
+      .then(response => response.json())
+      .then(data => {
+        setPedidos(data);
+        setFilteredPedidos(data);
       })
       .catch(error => console.error('Error al obtener los pedidos:', error));
   }, []);
@@ -27,20 +27,28 @@ export const HistorialPedidos: React.FC = () => {
     }
   };
 
-  const handleDownloadPdf = (pedido: IPedido) => {
-    const doc = new jsPDF();
-    doc.text(`Factura: ${pedido.factura.id}`, 10, 10);
-    doc.text(`Pedido N°: ${pedido.id}`, 10, 10);
-    doc.text(`Fecha del Pedido: ${pedido.fechaPedido}`, 10, 20);
-    doc.text(`Sucursal: ${pedido.sucursal.nombre}`, 10, 10);
-    doc.text(`SR : ${pedido.cliente.nombre+ " " +pedido.cliente.apellidos}`, 10, 10);
-    doc.text(`Pedido ID: ${pedido.id}`, 10, 10);
-    doc.text(`Detalle: ${pedido.detallesPedido}`, 10, 10);
-    doc.text(`Total: $${pedido.total}`, 10, 30);
-    doc.text(`Forma de pago: ${pedido.formaPago}`, 10, 20);
-    doc.text(`Tipo entrega: ${pedido.tipoEnvio}`, 10, 10);
-    doc.text(`Operador: ${pedido.empleado.nombre + " " +pedido.empleado.apellidos}`, 10, 10);
-    doc.save(`factura_pedido_${pedido.id}.pdf`);
+  const handleDownloadPdf = async (pedido: IPedido) => {
+    try {
+      const response = await fetch(`/factura/generar/${pedido.id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/pdf',
+        },
+      });
+      if (!response.ok) {
+        throw new Error('Error al descargar la factura');
+      }
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', `factura_pedido_${pedido.id}.pdf`);
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error('Error al descargar la factura:', error);
+    }
   };
 
   return (
@@ -75,5 +83,3 @@ export const HistorialPedidos: React.FC = () => {
     </Paper>
   );
 };
-
-
