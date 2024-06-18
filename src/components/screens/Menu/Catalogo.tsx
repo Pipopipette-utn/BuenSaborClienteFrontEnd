3f1b28e6f01f20b0ad4df172681d3692ad409cb2
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-06-18 15:07:39 -0300

    fix del fix: evitaba una request innecesaria el useCallback

--- src/components/screens/Menu/Catalogo.tsx
+++ src/components/screens/Menu/Catalogo.tsx
@@ -29,9 +29,12 @@ const Catalogo: React.FC = () => {
 
   const fetchArticulos = useFetchArticulos();
 
-  const handlePageChange = (_event: ChangeEvent<unknown>, value: number) => {
-    setPage(value);
-  };
+  const handlePageChange = useCallback(
+    (_event: ChangeEvent<unknown>, value: number) => {
+      setPage(value);
+    },
+    []
+  );
 
   const selectedCategoria = useAppSelector(
     (state: RootState) => state.selectedData.selectedCategoria
