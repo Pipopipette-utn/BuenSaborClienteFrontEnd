dba14de56768aff507527202dc3b64189fcfc171
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-06-23 13:43:31 -0300

    fix: Mejoras visuales modalDetalle

--- src/components/ui/ModalDetalle/ModalDetalle.tsx
+++ src/components/ui/ModalDetalle/ModalDetalle.tsx
@@ -32,6 +32,12 @@ export const ModalDetalle: React.FC<ModalDetalleProps> = ({
     return (articulo as IPromocion).precioPromocional !== undefined;
   };
 
+  const esManufacturado = (
+    articulo: IArticulo | IPromocion
+  ): articulo is IArticuloManufacturado => {
+    return (articulo as IArticuloManufacturado).descripcion !== undefined;
+  };
+
   return (
     <Modal open={open} onClose={handleClose}>
       <Box className={styles.modalBox}>
@@ -40,41 +46,42 @@ export const ModalDetalle: React.FC<ModalDetalleProps> = ({
           onClick={handleClose}
           sx={{
             position: "absolute",
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
+            right: 3,
+            top: -3,
+            color: "red",
           }}
         >
           <CloseIcon />
         </IconButton>
-        <Box>
-          {imageUrls.length > 1 ? (
-            <Carousel images={imageUrls} />
-          ) : (
-            <CardMedia
-              component="img"
-              className={styles.modalBoxMedia}
-              image={imageUrls[0]}
-              alt={articulo.denominacion}
-            />
-          )}
-        </Box>
+        {imageUrls.length > 1 ? (
+          <Carousel images={imageUrls} />
+        ) : (
+          <CardMedia
+            component="img"
+            className={styles.modalBoxMedia}
+            image={imageUrls[0]}
+            alt={articulo.denominacion}
+          />
+        )}
         <Typography
           variant="h3"
           component="h2"
-          sx={{ fontSize: { xs: "2rem" } }}
+          sx={{ fontSize: { xs: "2rem" }, paddingLeft: "16px" }}
         >
           {articulo.denominacion}
         </Typography>
         {/* Verifica si el artículo es IArticulo antes de acceder a sus propiedades específicas */}
-        {!esPromocion(articulo) && (
-          <Typography variant="h6" sx={{ mt: 2 }}>
+        {!esPromocion(articulo) && esManufacturado(articulo) && (
+          <Typography variant="h6" sx={{ mt: 2, paddingLeft: "16px" }}>
             {(articulo as IArticuloManufacturado).descripcion}
           </Typography>
         )}
 
         {/* Muestra el precio dependiendo del tipo de artículo */}
-        <Typography variant="h6" sx={{ mt: 2 }}>
+        <Typography
+          variant="h6"
+          sx={{ mt: 2, paddingLeft: "16px", paddingBottom: "16px" }}
+        >
           Precio: $
           {esPromocion(articulo)
             ? articulo.precioPromocional
